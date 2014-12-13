b5729d54e1e814e82cb2e6aa35a4628e32179486
hello20110562 <hello20110562@gmail.com>
2014-12-13 11:35:22 +0800

    test

---
+++ crazyjava以及语法/常用类/MathTest.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+public class MathTest{
+	public static void main(String[] args)
+	{
+		//。。。。。三角运算。。。。
+		//将弧度装换位角度
+		System.out.println("Math.toDegrees(1.57):"+Math.toDegrees(1.57));
+		
+		//将角度转换为弧度
+		System.out.println("Math.toRadians(80):"+Math.toRadians(80));
+		//计算正弦
+		System.out.println("Math.sin(30):"+Math.sin(30));
+	}
+
+}
