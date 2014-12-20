2992586d409280bbedd75a076c3bc0ca486aa3d4
hello20110562 <hello20110562@gmail.com>
2014-12-20 14:07:52 +0800

    ListTest

---
+++ LinkedListDemo.java
@@ -0,0 +1,23 @@
+package b_List;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		LinkedList ll = new LinkedList();		
+		
+		//1\
+		ll.addFirst("");
+		
+		
+		
+		
+
+	}
+
+}
